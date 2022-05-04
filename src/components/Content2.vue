1e9b1297972844864def7a8b4b95b591391ff8a4
Awaluddin Makkaraka, S.Pd, M.Pd <80614232+zaitun-edutech@users.noreply.github.com>
2022-05-05 05:53:46 +0800

    edit NISN -> NIS

--- src/components/Content2.vue
+++ src/components/Content2.vue
@@ -5,7 +5,10 @@
       <div class="row justify-content-center">
         <div class="col-lg-8">
           <h2>Silahkan Cek Kelulusan Di Form di Atas</h2>
-          <p class="lead">Caranya dengan memasukkan nilai NISN dan klik Cari</p>
+          <p class="lead">
+            Caranya dengan memasukkan nilai NIS(Nomor Induk sekolah) dan klik
+            Cari
+          </p>
           <p class="mb-0">
             Selamat Bagi Yang Lulus dan bagi yang tidak lulus untuk bersabar
             serta bisa kordinasi dengan sekolah Secepatnya Jika masih Ingin
